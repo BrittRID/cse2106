8dcac1215ac9080eb63562ab1914dce40ea0d71a
Brittany Ridley <britt.ridley@outlook.com>
2022-12-07 19:22:27 -0600

    Update file

--- game/constants.py
+++ game/constants.py
@@ -1,30 +1,31 @@
 import pygame
 import os
 
-WIDTH, HEIGHT = 1000, 900
+WIDTH, HEIGHT = 1000, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Robot Invasion")
 
 # Backgrounds
-BG_0 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_earth.png")), (WIDTH, HEIGHT))
-BG_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_road.png")), (WIDTH, HEIGHT))
-BG_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_city.png")), (WIDTH, HEIGHT))
-BG_3 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_rocket.png")), (WIDTH, HEIGHT))
-BG_4 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_clouds.png")), (WIDTH, HEIGHT))
-BG_5 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_hall.png")), (WIDTH, HEIGHT))
+BG_0 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106", "assets", "bg_earth.png")), (WIDTH, HEIGHT))
+# print("Why")
+BG_1 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106","assets", "bg_road.png")), (WIDTH, HEIGHT))
+BG_2 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106","assets", "bg_city.png")), (WIDTH, HEIGHT))
+BG_3 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106","assets", "bg_rocket.png")), (WIDTH, HEIGHT))
+BG_4 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106","assets", "bg_clouds.png")), (WIDTH, HEIGHT))
+BG_5 = pygame.transform.scale(pygame.image.load(os.path.join("cse2106","assets", "bg_hall.png")), (WIDTH, HEIGHT))
 
 # Enemy Robots
-RED_ROBOT = pygame.image.load(os.path.join("assets", "robot_red.png"))
-GREEN_ROBOT = pygame.image.load(os.path.join("assets", "robot_green.png"))
-BLUE_ROBOT = pygame.image.load(os.path.join("assets", "robot_blue.png"))
-YELLOW_ROBOT = pygame.image.load(os.path.join("assets", "robot_yellow.png"))
+RED_ROBOT = pygame.image.load(os.path.join("cse2106","assets", "robot_red.png"))
+GREEN_ROBOT = pygame.image.load(os.path.join("cse2106","assets", "robot_green.png"))
+BLUE_ROBOT = pygame.image.load(os.path.join("cse2106","assets", "robot_blue.png"))
+YELLOW_ROBOT = pygame.image.load(os.path.join("cse2106","assets", "robot_yellow.png"))
 
 # Balls
-RED_BALL = pygame.image.load(os.path.join("assets", "ball_red.png"))
-GREEN_BALL = pygame.image.load(os.path.join("assets", "ball_green.png"))
-BLUE_BALL = pygame.image.load(os.path.join("assets", "ball_blue.png"))
-YELLOW_BALL = pygame.image.load(os.path.join("assets", "ball_yellow.png"))
+RED_BALL = pygame.image.load(os.path.join("cse2106","assets", "ball_red.png"))
+GREEN_BALL = pygame.image.load(os.path.join("cse2106","assets", "ball_green.png"))
+BLUE_BALL = pygame.image.load(os.path.join("cse2106","assets", "ball_blue.png"))
+YELLOW_BALL = pygame.image.load(os.path.join("cse2106","assets", "ball_yellow.png"))
 
 # Player Cowboy
-COWBOY = pygame.image.load(os.path.join("assets", "cowboy.png"))
-BLACK_BALL = pygame.image.load(os.path.join("assets", "ball_black.png"))
+COWBOY = pygame.image.load(os.path.join("cse2106","assets", "cowboy.png"))
+BLACK_BALL = pygame.image.load(os.path.join("cse2106","assets", "ball_black.png"))
